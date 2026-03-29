e0801f983a96605d344e7bb2f69e6e3e36ce8163
MSarhan10 <130199705+MSarhan10@users.noreply.github.com>
2026-03-29 19:34:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ============================================================
 //  CACHE NAMES
 // ============================================================
-const APP_CACHE    = 'mirimate-v87';
+const APP_CACHE    = 'mirimate-v88';
 const STATIC_CACHE = 'mirimate-static-v1';
 const TEMP_CACHE   = 'mirimate-temp';      // ← permanent name, never deleted
 
